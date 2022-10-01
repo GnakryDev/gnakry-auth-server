e5864cc84a59e53a93324963ce7898f1cda010e2
Boubacar Sidy Diallo <bsdiallo07@gmail.com>
2022-10-01 23:30:42 +0200

    IP validator

--- src/gnakry_auth_server/main.py
+++ src/gnakry_auth_server/main.py
@@ -7,6 +7,11 @@ import json
 import requests
 from datetime import datetime, timezone
 import yaml
+import logging
+# from IPQualityScore.DBReader import DBReader
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def get_app():
@@ -20,7 +25,7 @@ app.include_router(router)
 
 
 @app.get("/auth/basic")
-def user_auth(credentials: HTTPBasicCredentials = Depends(security)):
+def user_auth(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
     valid_user = False
     with open("config/users_list.yml") as file:
         user_list = yaml.load(file, Loader=yaml.FullLoader)
@@ -29,6 +34,43 @@ def user_auth(credentials: HTTPBasicCredentials = Depends(security)):
             valid_user = True
             break
     if valid_user:
-        return {"auth": "OK"}
+        return {"client_host": client_host, "auth": "OK"}
     else:
         raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+def ipValidator(request: Request):
+    client_host = request.client.host
+    print("################################################## ")
+    print("################################################## ")
+    # u = DBReader(
+    #     "IPQualityScore-IP-Reputation-Database-IPv4.ipqs").Fetch(request.headers.get(
+    #         "x-forwarded-for"))
+    # print("IsVPN: " + u.IsVPN())
+    # print("IsProxy: " + u.IsProxy())
+    # print("ActiveVPN: " + u.ActiveVPN())
+    # print("Country: " + u.Country())
+    # print("City: " + u.City())
+    # print("ISP: " + u.ISP())
+    # print("Organization: " + u.Organization())
+    # print("IsCrawler: " + u.IsCrawler())
+    print(" ################################################## ")
+    print("################################################## ")
+    print({"client_host": client_host, "auth": "OK"})
+    print("################################################## ")
+    print("################################################## ")
+    params = ['query', 'status', 'country',
+              'countryCode', 'city', 'timezone', 'mobile']
+    resp = requests.get('http://ip-api.com/json/' + request.headers.get(
+        "x-forwarded-for"), params={'fields': ','.join(params)})
+    # read response as JSON (converts to Python dict)
+    info = resp.json()
+    # display the response
+    print(info)
+    print("################################################## ")
+    print("################################################## ")
+    print(request.headers)
+    print("################################################## ")
+    print("################################################## ")
+    print("################################################## ")
+    return True
